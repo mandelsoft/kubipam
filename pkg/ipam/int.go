e414ff4cf12c6b13f7a535092e9d7ffef94ad533
Uwe Krueger <uwe.krueger@sap.com>
2021-08-26 20:28:09 +0200

    request for #ips

--- pkg/ipam/int.go
+++ pkg/ipam/int.go
@@ -19,6 +19,7 @@
 package ipam
 
 import (
+	"fmt"
 	"math/big"
 )
 
@@ -27,6 +28,15 @@ type Int big.Int
 var IntOne = Int64(1)
 var IntZero = Int64(0)
 var IntTwoFiveFive = Int64(255)
+
+func ParseInt(s string) (Int, error) {
+	n := new(big.Int)
+	n, ok := n.SetString(s, 10)
+	if !ok {
+		return IntZero, fmt.Errorf("invalid number")
+	}
+	return Int(*n), nil
+}
 
 func Int64(i int64) Int {
 	return Int(*big.NewInt(i))
